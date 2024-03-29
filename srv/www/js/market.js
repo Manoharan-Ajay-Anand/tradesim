be2e91414fb56e28b82f4c1a4ba058e3d397fa3b
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-03-29 23:41:08 +0800

    create price box

--- srv/www/js/market.js
+++ srv/www/js/market.js
@@ -7,27 +7,34 @@ if (!marketId || !traderId) {
     alert("MarketId and TraderId not specified in query string");
 }
 
-const evtSource = new EventSource("/api/stream/" + marketId + "/" + traderId);
+// const evtSource = new EventSource("/api/stream/" + marketId + "/" + traderId);
 
-evtSource.addEventListener("pricePoint", (event) => {
-    const pp = JSON.parse(event.data);
-    console.log(pp);
-});
+// evtSource.addEventListener("pricePoint", (event) => {
+//     const pp = JSON.parse(event.data);
+//     console.log(pp);
+// });
 
-evtSource.addEventListener("duplicate", () => {
-    alert("Only one stream allowed per trader");
-    evtSource.close();
-});
+// evtSource.addEventListener("duplicate", () => {
+//     alert("Only one stream allowed per trader");
+//     evtSource.close();
+// });
 
-const bidForm = { marketId: marketId, traderId: traderId, type: "bid", price: 100, quantity: 10 };
-const askForm = { marketId: marketId, traderId: traderId, type: "ask", price: 100, quantity: 10 };
+const priceTable = document.getElementById("priceTable");
+for (let i = 1; i <= 50; ++i) {
+    const row = document.createElement("tr");
 
-async function sendOrders() {
-    const bidReq = new Request("/api/order", { method: "POST", body: JSON.stringify(bidForm) });
-    const bidRes = await fetch(bidReq);
-    console.log(bidRes);
-    const askReq = new Request("/api/order", { method: "POST", body: JSON.stringify(askForm) });
-    const askRes = await fetch(askReq);
+    const bid = document.createElement("td");
+    bid.classList.add("invisible", "cell");
+    bid.textContent = "0";
+
+    const price = document.createElement("td");
+    price.classList.add("cell");
+    price.textContent = i.toString();
+
+    const ask = document.createElement("td");
+    ask.classList.add("invisible", "cell");
+    
+    row.append(bid, price, ask);
+    priceTable.prepend(row);
 }
 
-setTimeout(sendOrders, 3000);
